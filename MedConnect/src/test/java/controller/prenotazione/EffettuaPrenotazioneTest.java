a7b641c2bf94ad44bcc8553a5dbd53b335fb5263
VINCENZO MEDICA <v.medica@studenti.unisa.it>
2025-01-06 09:12:30 +0100

    Aggiunta di altri test ad effettua prenotazione

--- MedConnect/src/test/java/controller/prenotazione/EffettuaPrenotazioneTest.java
+++ MedConnect/src/test/java/controller/prenotazione/EffettuaPrenotazioneTest.java
@@ -39,7 +39,7 @@ public class EffettuaPrenotazioneTest {
     }
 
     @Test
-    void testDoPost_Failure() throws Exception {
+    void testDoPost_FailureCreazionePrenotazione() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         RequestDispatcher dispatcher = mock(RequestDispatcher.class);
@@ -63,4 +63,79 @@ public class EffettuaPrenotazioneTest {
         }
     }
 
+
+    @Test
+    void testDoPost_FailureParametroNotaPrenotazione() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+
+        try (MockedStatic<PrenotazioneDAO> mockedDAO = mockStatic(PrenotazioneDAO.class)) {
+            mockedDAO.when(() -> PrenotazioneDAO.doSave(any(Prenotazione.class))).thenReturn(-1);
+
+            EffettuaPrenotazione servlet = new EffettuaPrenotazione();
+
+            when(request.getParameter("nota")).thenReturn(null);
+            when(request.getParameter("idPaziente")).thenReturn("123");
+            when(request.getParameter("idDisponibilita")).thenReturn("456");
+
+            when(request.getRequestDispatcher("/WEB-INF/results/error.jsp")).thenReturn(dispatcher);
+
+            servlet.doPost(request, response);
+
+            verify(request).setAttribute("message", "Errore nei parametri.");
+
+            verify(dispatcher).forward(request, response);
+        }
+    }
+
+    @Test
+    void testDoPost_FailureParametroIdPazientePrenotazione() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+
+        try (MockedStatic<PrenotazioneDAO> mockedDAO = mockStatic(PrenotazioneDAO.class)) {
+            mockedDAO.when(() -> PrenotazioneDAO.doSave(any(Prenotazione.class))).thenReturn(-1);
+
+            EffettuaPrenotazione servlet = new EffettuaPrenotazione();
+
+            when(request.getParameter("nota")).thenReturn("Nota di test");
+            when(request.getParameter("idPaziente")).thenReturn("-1");
+            when(request.getParameter("idDisponibilita")).thenReturn("456");
+
+            when(request.getRequestDispatcher("/WEB-INF/results/error.jsp")).thenReturn(dispatcher);
+
+            servlet.doPost(request, response);
+
+            verify(request).setAttribute("message", "Errore nei parametri.");
+
+            verify(dispatcher).forward(request, response);
+        }
+    }
+
+    @Test
+    void testDoPost_FailureParametroIdDisponibilitaPrenotazione() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+
+        try (MockedStatic<PrenotazioneDAO> mockedDAO = mockStatic(PrenotazioneDAO.class)) {
+            mockedDAO.when(() -> PrenotazioneDAO.doSave(any(Prenotazione.class))).thenReturn(-1);
+
+            EffettuaPrenotazione servlet = new EffettuaPrenotazione();
+
+            when(request.getParameter("nota")).thenReturn(null);
+            when(request.getParameter("idPaziente")).thenReturn("123");
+            when(request.getParameter("idDisponibilita")).thenReturn("-1");
+
+            when(request.getRequestDispatcher("/WEB-INF/results/error.jsp")).thenReturn(dispatcher);
+
+            servlet.doPost(request, response);
+
+            verify(request).setAttribute("message", "Errore nei parametri.");
+
+            verify(dispatcher).forward(request, response);
+        }
+    }
 }
